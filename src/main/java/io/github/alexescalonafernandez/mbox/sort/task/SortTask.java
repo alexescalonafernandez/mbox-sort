4f6b9763a5cb7b3b7ae3b7d93ea2f353551bf56b
Alexander Escalona Fernández <alexescalonafernandez@gmail.com>
2018-09-28 19:02:35 -0400

    refactor: get date of 'From' line with regex

--- src/main/java/io/github/alexescalonafernandez/mbox/sort/task/SortTask.java
+++ src/main/java/io/github/alexescalonafernandez/mbox/sort/task/SortTask.java
@@ -4,10 +4,13 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -70,23 +73,104 @@ public class SortTask implements Runnable{
     }
 
     private void processLine(final String line) throws IOException {
-        Pattern fromPattern = Pattern.compile("^From\\s\\d");
-        Pattern datePattern = Pattern.compile("(\\w{3} \\w{3} \\d{2} \\d{2}:\\d{2}:\\d{2} .0000 \\d{4})$");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH);
-        Matcher matcher;
+        Pattern fromPattern = Pattern.compile("^From\\s+[^\\s]+\\s+(.+)$");
+        Matcher matcher = fromPattern.matcher(line);
         long serialize;
-        if(fromPattern.matcher(line).find()) {
-            tryCloseWriter();
-            matcher = datePattern.matcher(line);
-            if(matcher.find()) {
-                LocalDateTime dateTime = LocalDateTime.parse(matcher.group(1), formatter);
+        if(matcher.find()) {
+            try {
+                LocalDateTime dateTime = tryGetDate(matcher.group(1));
+                tryCloseWriter();
                 serialize = dateTime.toEpochSecond(ZoneOffset.UTC);
                 File file = File.createTempFile(String.valueOf(serialize), ".tmp");
                 file.deleteOnExit();
                 sortFileNotifier.accept(serialize, file);
                 writer = new PrintWriter(new FileOutputStream(file, true), true);
+            } catch (Exception ex) {
+                System.out.println(line);
             }
         }
         Optional.ofNullable(writer).ifPresent(pw -> pw.print(line));
+    }
+
+    private LocalDateTime tryGetDate(String toMatch) {
+        final StringBuilder builder = new StringBuilder(toMatch);
+        final HashMap<String, String> fields = new HashMap<>();
+
+        final Function<Matcher, String> mapper = m -> {
+            String value = m.group();
+            builder.replace(m.start(), m.end(), "");
+            return value;
+        };
+
+        fields.put("zone", Optional.ofNullable(Pattern.compile("(?:\\B)[\\+-]\\d{4}"))
+                .map(pattern -> pattern.matcher(builder.toString()))
+                .filter(m -> m.find())
+                .map(mapper)
+                .orElse("+0000")
+        );
+
+        Optional.ofNullable(Pattern.compile("\\b(\\d{2}):(\\d{2}):?(\\d{2})?\\b"))
+                .map(pattern -> pattern.matcher(builder.toString()))
+                .filter(m -> m.find())
+                .map(m -> {
+                    String value = m.group();
+                    fields.put("hour", m.group(1));
+                    fields.put("minute", m.group(2));
+                    fields.put("second", Optional.ofNullable(m.group(3)).orElse("00"));
+                    builder.replace(m.start(), m.end(), "");
+
+                    return value;
+                })
+                .orElse(null);
+
+        fields.put("year", Optional.ofNullable(Pattern.compile("\\b\\d{4}\\b"))
+                .map(pattern -> pattern.matcher(builder.toString()))
+                .filter(m -> m.find())
+                .map(mapper)
+                .orElse(null)
+        );
+
+        fields.put("day", Optional.ofNullable(Pattern.compile("\\b\\d{2}\\b"))
+                .map(pattern -> pattern.matcher(builder.toString()))
+                .filter(m -> m.find())
+                .map(mapper)
+                .orElse(null)
+        );
+
+        fields.put("monthName", Optional.ofNullable(Pattern.compile("\\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\b"))
+                .map(pattern -> pattern.matcher(builder.toString()))
+                .filter(m -> m.find())
+                .map(mapper)
+                .orElse(null)
+        );
+
+        fields.put("monthNumber", Optional.ofNullable(Pattern.compile("\\b\\d{2}\\b"))
+                .map(pattern -> pattern.matcher(builder.toString()))
+                .filter(m -> m.find())
+                .map(mapper)
+                .orElse(null)
+        );
+
+        Optional.ofNullable(fields.get("monthName"))
+                .ifPresent(monthName -> {
+                    int value = Arrays.asList("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+                            .indexOf(monthName) + 1;
+                    String str = String.valueOf(value);
+                    if(value < 10)
+                        str = "0" + value;
+                    fields.put("monthNumber", str);
+                });
+        builder.delete(0, builder.length());
+
+        builder.append(fields.get("day")).append(" ")
+                .append(fields.get("monthNumber")).append(" ")
+                .append(fields.get("year")).append(" ")
+                .append(fields.get("hour")).append(":")
+                .append(fields.get("minute")).append(":")
+                .append(fields.get("second")).append(" ")
+                .append(fields.get("zone"));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy HH:mm:ss Z", Locale.ENGLISH);
+        return LocalDateTime.parse(builder.toString(), formatter);
     }
 }
